f12d5bcd3931d9a86bd0df4174cbd782ad64ae28
Diogo Merelles <diogo.merelles.faria@gmail.com>
2022-12-20 15:27:59 +0000

    design fixes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Header.css'
 import { faIdCard, faRightToBracket, faUser, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -6,6 +6,10 @@ import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
     let navigate = useNavigate();
+    const [searchIsActive, setSearchIsActive] = useState(false);
+    const toggleSearch = () => {
+        setSearchIsActive(!searchIsActive);
+    }
   return (
     <div className='header-container'>
         <div className="header-categories">Blogs</div>
@@ -17,7 +21,7 @@ const Header = () => {
             <FontAwesomeIcon className='nav-icon' icon={faIdCard} onClick={() => setTimeout(() =>{navigate("/userProfile")}, 500)} />
             <FontAwesomeIcon className='nav-icon' icon={faRightToBracket} onClick={() => setTimeout(() =>{navigate("/login")}, 500)} />
             <FontAwesomeIcon className='nav-icon' icon={faUser} onClick={() => setTimeout(() =>{navigate("/register")}, 500)} />
-            <FontAwesomeIcon className='nav-icon' icon={faMagnifyingGlass} />
+            <FontAwesomeIcon className='nav-icon' icon={faMagnifyingGlass} onClick={searchIsActive} />
             {/* user profile / login / register / search */}
         </div>
     </div>
