78786ee61405f419f5da7190837156a6d0d6de06
Diogo Merelles <diogo.merelles.faria@gmail.com>
2022-12-20 10:56:09 +0000

    added react-router-dom routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import Home from './Containers/Home/Home';
+import AddBlog from './Containers/AddBlog/AddBlog';
+import Blog from './Containers/Blog/Blog';
+import Login from './Containers/Login.jsx/Login';
+import Register from './Containers/Register/Register';
+import UserProfile from './Containers/UserProfile/UserProfile';
+import ErrNotFound from './Containers/ErrNotFound/ErrNotFound';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h2>Blog</h2>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/addBlog" element={<AddBlog />} />
+        <Route path="/editBlog/:id" element={<AddBlog />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/userProfile" element={<UserProfile />} />
+        <Route path="*" element={<ErrNotFound />} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
