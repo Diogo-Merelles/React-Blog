b2ea6bcbfc43d9c775e0488b0829f45528b88a1f
Diogo Merelles <diogo.merelles@xing.com>
2023-04-20 13:42:32 +0100

    created test folders, and installed jest/react-testing dependencies

---
+++ src/Containers/Home/__test__/Home.test.js
@@ -0,0 +1,6 @@
+import {render, screen} from '@testing-library/react'
+import SearchBar from '../../../Components/SearchBar/SearchBar'
+
+test('renders the home page', () => {
+    render(<SearchBar/>)
+});
